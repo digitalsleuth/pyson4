30fa77a0b123f735f6d8e1a27aaffaad26980923
Fetchered <10457978+Fetchered@users.noreply.github.com>
2019-12-01 04:25:56 +0000

    Prettier output for json, error handling

--- pyson4.py
+++ pyson4.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 
 """
-The Firefox jsonlz4 'file-type' is unique in that the header (mozLz40\x00) is not a proper LZ4 frame.
+This is a Python 3 script for converting the Firefox lz4 type file header to readable JSON.
+The Firefox jsonlz4 'file-type' is unique in that the header (mozLz40\x00) is not a standard LZ4 frame type.
 Due to this, the first 8 bytes (the header) must be skipped, and the remainder of the file is treated as a block.
 
 This script will essentially take an input file (jsonlz4 type file), read the block, and output to a json file.
-Requirements: pip install python-lz4
-
+Requirements: pip install lz4
+Windows requirement: lz4 requires Visual Studio Build Tools 14 + to install 
 Source Material:
 
 https://dxr.mozilla.org/mozilla-central/source/toolkit/components/lz4/lz4.js
 https://buildmedia.readthedocs.org/media/pdf/python-lz4/latest/python-lz4.pdf
-
-TODO:
-
-Might add option to ignore incorrect header and attempt to parse as block.
 """
 import lz4.block
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
+import json
+import os.path
 
 __author__ = 'Corey Forman'
-__date__ = '28 Nov 2019'
-__version__ = '0.2'
-__description__ = 'Firefox JSONLZ4 Parsing Tool'
+__date__ = '30 Nov 2019'
+__version__ = '1.0'
+__description__ = 'Firefox JSON LZ4 Parsing Tool'
 
 class LZ4Error(Exception):
     pass
@@ -39,19 +38,22 @@ def decompress(jlz4_file):
     jsonlz4_header = b'mozLz40\x00'
     if jlz4_file.read(8) != jsonlz4_header:
         raise HeaderMismatch("Invalid jsonlz4 Header")
-    return lz4.block.decompress(jlz4_file.read())
+    extracted = lz4.block.decompress(jlz4_file.read())
+    json_load = json.loads(extracted)
+    return json_load
 
 def compress(file_block):
     compressed_block = lz4.block.compress(file_block.read())
     return b'mozLz40\x00' + compressed_block
 
 if __name__ == "__main__":
-    arg_parse = ArgumentParser(description='Firefox JSONLZ4 Compression/Decompression tool.', epilog='This tool will only work with the .jsonlz4 format with Firefox.\nFor other LZ4 utilities, try the python-lz4 library')
-    arg_parse.add_argument("input_file", metavar="<input_file>", help="Input file, including path if necessary")
-    arg_parse.add_argument("output_file", metavar="<output_file>", help="Output file name, including path if necessary")
+    arg_parse = ArgumentParser(description="Firefox JSONLZ4 parsing tool", epilog="This tool will only work with the Mozilla Firefox lz4 format with Firefox.\nThis includes the jsonlz4 and mozlz4 file extensions.\nFor other LZ4 utilities, try the python-lz4 library", formatter_class=RawTextHelpFormatter)
+    arg_parse.add_argument("input_file", metavar="<input_file>", help="Input file including path (if necessary)")
+    arg_parse.add_argument("output_file", metavar="<output_file>", help="Output file name including path (if necessary)")
     arg_parse.add_argument("-d", action="store_true", help="Decompress the input file.")
     arg_parse.add_argument("-c", action="store_true", help="Compress the input file.")
-    arg_parse.add_argument("-v", action="version", version='%(prog)s' +str(__version__))
+    arg_parse.add_argument("-v", action="version", version='%(prog)s' +' v' + str(__version__))
+    arg_parse.add_argument("--verbose", action="store_true", help="Verbose - prints output path and size.\nBetter to not use if iterating through files")
     args = arg_parse.parse_args()
 
     try:
@@ -61,7 +63,7 @@ if __name__ == "__main__":
         raise SystemExit(1)
     
     try:
-        output_file = open(args.output_file, 'wb')
+        output_file = open(args.output_file, 'w')
     except IOError as e:
         print("Unable to write to '%s': %s" % (args.output_file, e), file=sys.stderr)
         raise SystemExit(1)
@@ -69,20 +71,35 @@ if __name__ == "__main__":
     try:
         if args.d:
             blob = decompress(input_file)
-        else:
+            try:
+                json.dump(blob, output_file, indent=4, separators=(',',': '))
+                output_file.write('\n')
+            except IOError as e:
+                print("Unable to write to '%s': %s" % (args.output_file, e), file=sys.stderr)
+                raise SystemExit(1)
+            else:
+                output_file.close()
+                file_size = int(os.path.getsize(args.output_file))
+                if args.verbose:
+                    print("Output file '%s' created. %s bytes output." % (args.output_file, file_size))
+            finally:
+                output_file.close()                      
+        elif args.c:
             blob = compress(input_file)
+            try:
+                output_file = open(args.output_file, 'wb')
+                output_file.write(blob)
+            except IOError as e:
+                print("Unable to write to '%s': %s" % (args.output_file, e), file=sys.stderr)
+                raise SystemExist(1)
+            else:
+                output_file.close()
+                file_size = int(os.path.getsize(args.output_file))
+                if args.verbose:
+                    print("Output file '%s' created. %s bytes output." % (args.output_file, file_size))
+        else:
+            print("Please choose either the -d (decompress) or -c (compress) option to proceed.")
+            raise SystemExit(0)
     except Exception as e:
         print("Unable to perform operations on file '%s': %s" % (args.input_file, e), file=sys.stderr)
         raise SystemExit(1)
-
-    try:
-        output_file.write(blob)
-        file_size = len(blob)
-    except IOError as e:
-        print("Unable to write to '%s': %s" % (args.output_file, e), file=sys.stderr)
-        raise SystemExit(1)
-    else:
-        print("File '%s' created. Resulting file is %s bytes, and is in JSON format." % (args.output_file, file_size))
-    finally:
-        output_file.close()
-
